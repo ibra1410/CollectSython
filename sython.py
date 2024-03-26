45d900253dc4b23cb0f28d63ca8e19b7902a2441
ibra1410 <160061682+ibra1410@users.noreply.github.com>
2024-03-26 10:15:02 +0100

    Update sython.py

--- sython.py
+++ sython.py
@@ -2,65 +2,29 @@ import os
 import json
 import requests
 import subprocess
-A = '\033[1;34m'#ازرق
-X = '\033[1;33m' #اصفر
+
+A = '\033[1;34m'  # ازرق
+X = '\033[1;33m'  # اصفر
+
+# إدخال توكن البوت والايدي مباشرة
+api_id = 27455984
+api_hash = '62d5f68ce2e9189636967120220f5755'
+bot_token = '6615217281:AAG-kS0Mwo22uPvtOL4aLwxAuymAyitHnfo'
+DEVLOO = '6946908675'
+MAX_ACCOUNTS = 10
 
 filename = 'sython.json'
 
-try:
-    with open(filename, 'r') as f:
-        data = json.load(f)
-        api_id = data['api_id']
-        api_hash = data['api_hash']
-        bot_token = data['bot_token']
-        DEVLOO = data['DEVLOO']
-        MAX_ACCOUNTS = data['MAX_ACCOUNTS']
-        id_bot = bot_token.split(':')[0]  # Extract id_bot from bot_token
+data = {
+    'api_id': api_id,
+    'api_hash': api_hash,
+    'bot_token': bot_token,
+    'DEVLOO': DEVLOO,
+    'MAX_ACCOUNTS': MAX_ACCOUNTS
+}
 
-        # Send a GET request to the Telegram API
-        response = requests.get(f'https://api.telegram.org/bot{bot_token}/getme')
-        response_data = response.json()
-
-        # Extract bot_username from the response
-        user_bot = response_data['result']['username']
-except FileNotFoundError:
-    api_id = 23398930
-    print('  ')
-    api_hash = 'bd3e85a7aae40566f2fa8804d200d6d0'
-    print('  ')
-    bot_token = input(A+"❖ Inter Your Token ➜  "+X)
-    print('  ')
-    DEVLOO = input(A+"❖ Inter Id Off Controller Account ➜  "+X)
-    print('  ')
-    MAX_ACCOUNTS = int(input(A+"❖ Inter Num Of Max Acc ➜  "+X))
-    print('  ')
-    id_bot = bot_token.split(':')[0]  # Extract id_bot from bot_token
-    print('  ')
-
-    # Send a GET request to the Telegram API
-    response = requests.get(f'https://api.telegram.org/bot{bot_token}/getme')
-    response_data = response.json()
-
-    # Extract bot_username from the response
-    user_bot = response_data['result']['username']
-    
-    print('  ')
-    
-    data = {
-        'api_id': api_id,
-        'api_hash': api_hash,
-        'bot_token': bot_token,
-        'DEVLOO': DEVLOO,
-        'MAX_ACCOUNTS': MAX_ACCOUNTS,
-        'user_bot': user_bot,
-        'id_bot': id_bot
-    }
-    
-    with open(filename, 'w') as f:
-        json.dump(data, f)
-
-
-
+with open(filename, 'w') as f:
+    json.dump(data, f)
 
 response = requests.get("https://raw.githubusercontent.com/sythontm/CollectSython/main/sython-telethon-cl.py")
 
